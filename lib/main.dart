70974c5cfc7d361e1975754625e7c4e1315b43d7
Gagandeep39 <singh.gagandeep3911@gmail.com>
2019-05-05 23:40:45 +0530

    Fixed Firebase List view not appearing

--- lib/main.dart
+++ lib/main.dart
@@ -94,32 +94,31 @@ class _MyHomePageState extends State<MyHomePage> {
                     },
                   ),
 
-                  Flexible(
-                    child: FirebaseAnimatedList(
-                      query: null,
-                      itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation animation, int index) {
-                        return Card(
-                          child: ListTile(
-                            leading: CircleAvatar(
-                              backgroundColor: Colors.blue,
-                            ),
-                            title: Text(boardMessages[index].subject),
-                            subtitle: Text(boardMessages[index].body),
-                          ),
-                        );
-                      },
-                    ),
-                  )
                 ],
               ),
+            ),
+          ),
+
+          Flexible(
+            child: FirebaseAnimatedList(
+              query: databaseReference,
+              itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation animation, int index) {
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.blue,
+                    ),
+                    title: Text(boardMessages[index].subject),
+                    subtitle: Text(boardMessages[index].body),
+                  ),
+                );
+              },
             ),
           )
         ],
       ),
     );
   }
-
-
 
   void  _onEntryAdded(Event event) {
     setState(() {
@@ -137,59 +136,3 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 }
-
-
-//class MyHomePage extends StatefulWidget {
-//  MyHomePage({Key key, this.title}) : super(key: key);
-//  final String title;
-//
-//  @override
-//  _MyHomePageState createState() => _MyHomePageState();
-//}
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//  int _counter = 0;
-//
-//  void _incrementCounter() {
-//
-//    database.reference().child("message").set({
-//      "artist":"Troye Sivan",
-//      "song":"Bite"
-//    });
-//    setState(() {
-//      database.reference().child("message").once().then((DataSnapshot snapshot){
-//        Map<dynamic, dynamic> map = snapshot.value;
-//        print(map.values);
-//      });
-//      _counter++;
-//    });
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text(widget.title),
-//      ),
-//      body: Center(
-//        child: Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            Text(
-//              'You have pushed the button this many times:',
-//            ),
-//            Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.display1,
-//            ),
-//          ],
-//        ),
-//      ),
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: _incrementCounter,
-//        tooltip: 'Increment',
-//        child: Icon(Icons.add),
-//      ), // This trailing comma makes auto-formatting nicer for build methods.
-//    );
-//  }
-//}
